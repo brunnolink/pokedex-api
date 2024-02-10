77a62e9784f647936762d2e96cd081232e3d6d4d
brunno <brunnolinker@gmail.com>
2024-02-09 23:22:37 -0300

    Desenvolvimento do consumo da PokeAPI

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMoreButton');
+const limit = 10;
+const maxRecords = 151;
+let offset = 0;
+
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => ` 
+        <li class="pokemon ${pokemon.type}">
+        <span class="number">#${pokemon.number}</span>
+        <span class="name">${pokemon.name}</span>
+    
+        <div class="detail">
+            <ol class="types">
+               ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+            </ol>
+            <img src="${pokemon.photo}">
+        </div>
+    </li>`).join('');
+        pokemonList.innerHTML += newHtml; 
+    })
+ }
+
+ loadPokemonItens(offset, limit);
+
+ loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecord = offset + limit;
+
+    if (qtdRecord >= maxRecords ) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+        loadMoreButton.parentElement.removeChild(loadMoreButton) 
+    }else { loadPokemonItens(offset, limit);}
+   
+}) 
